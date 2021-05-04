0b79f7257051e3db250573b1d9ae2c190f988165
unknown <vrnakano@gmail.com>
2021-05-04 17:37:38 -0300

    Classe Produtores e New Order

--- projeto/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ projeto/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -7,13 +7,16 @@ public class NewOrderMain {
     public static void main(String[] args) {
 
         var producer = new KafkaProducer<String, String>(properties());
-
+        var value = "123","155","1555";
+        var record = new ProducerRecord<>(topic:"ECOMMERCE_NEW_ORDER", value, value);
+        producer.send(record);
     }
 
     private static Properties properties() {
         var properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, )
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
     }
-
-
 }
